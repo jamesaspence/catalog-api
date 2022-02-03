af4a1b7af7470519747c176ebd7773c05a151903
James Spence <jamesaspence20@gmail.com>
2022-02-03 00:21:49 -0600

    feat: ApiTokenProvider

---
+++ app/Auth/ApiTokenProvider.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Auth;
+
+
+use App\Models\ApiToken;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ApiTokenProvider
+{
+    private ?Request $request = null;
+    private ?ApiToken $apiToken = null;
+
+    public function setRequest(Request $request): void
+    {
+        $this->request = $request;
+        $this->apiToken = null;
+    }
+
+    public function getApiToken(): ?ApiToken
+    {
+        if (is_null($this->request)) {
+            return null;
+        }
+
+        if (!is_null($this->apiToken)) {
+            return $this->apiToken;
+        }
+
+        return $this->apiToken = $this->decodeToken($this->request);
+    }
+
+    private function decodeToken(Request $request): ?ApiToken
+    {
+        $token = $request->bearerToken();
+
+        if (empty($token)) {
+            return null;
+        }
+
+        $decoded = base64_decode($token);
+        if (!Str::contains($decoded, ':')) {
+            return null;
+        }
+
+        $exploded = explode(':', $decoded);
+
+        if (count($exploded) !== 2) {
+            return null;
+        }
+
+        /** @var ?ApiToken $apiToken */
+        $apiToken = ApiToken::query()
+            ->where('client_id', '=', $exploded[0])
+            ->first();
+
+        if (is_null($apiToken)) {
+            return null;
+        }
+
+        if (!Hash::check($exploded[1], $apiToken->token)) {
+            return null;
+        }
+
+        return $apiToken;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Auth\ApiTokenProvider;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -17,6 +21,22 @@ class AuthServiceProvider extends ServiceProvider
     ];
 
     /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        parent::register();
+
+        $this->app->singleton(ApiTokenProvider::class, function (Application $application) {
+            $apiTokenProvider = new ApiTokenProvider();
+
+            $application->refresh('request', $apiTokenProvider, 'setRequest');
+
+            return $apiTokenProvider;
+        });
+    }
+
+    /**
      * Register any authentication / authorization services.
      *
      * @return void
@@ -24,7 +44,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
