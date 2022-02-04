9df27f1544c2ff1fb48b156adecc300b4ceec850
James Spence <jamesaspence20@gmail.com>
2022-02-04 00:46:13 -0600

    feat: max file size of 4mb

--- app/Http/Requests/UploadGifRequest.php
+++ app/Http/Requests/UploadGifRequest.php
@@ -24,7 +24,7 @@ class UploadGifRequest extends FormRequest
     public function rules()
     {
         return [
-            'image' => 'required|image|mimes:gif'
+            'image' => 'required|image|mimes:gif|max:4096'
         ];
     }
 }
