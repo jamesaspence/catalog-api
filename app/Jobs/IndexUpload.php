9014407122ff10575a0bcf48a3597bed065b9b58
James Spence <jamesaspence20@gmail.com>
2022-02-06 14:46:47 -0600

    feat: don't index same upload more than once

--- app/Jobs/IndexUpload.php
+++ app/Jobs/IndexUpload.php
@@ -9,6 +9,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 
 class IndexUpload implements ShouldQueue
@@ -25,6 +26,16 @@ class IndexUpload implements ShouldQueue
     public function __construct(Upload $upload)
     {
         $this->upload = $upload;
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     *
+     * @return array
+     */
+    public function middleware(): array
+    {
+        return [(new WithoutOverlapping($this->upload->id))->dontRelease()];
     }
 
     /**
