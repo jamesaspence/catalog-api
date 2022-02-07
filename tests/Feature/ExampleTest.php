be2e660f28d9dfd4c04007d459880b5928870201
James Spence <jamesaspence20@gmail.com>
2022-02-06 22:38:46 -0600

    cI: added return type to test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function testExample()
+    public function testExample(): void
     {
         $this->assertTrue(true);
     }
