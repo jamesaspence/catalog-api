261910ecca95f835ef9762298bd4eff3a8008d40
James Spence <jamesaspence20@gmail.com>
2022-02-06 21:09:08 -0600

    refactor: remove unnecessary FQNs

--- app/Http/Middleware/AuthenticateApiToken.php
+++ app/Http/Middleware/AuthenticateApiToken.php
@@ -4,7 +4,9 @@ namespace App\Http\Middleware;
 
 use App\Auth\ApiTokenProvider;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthenticateApiToken
 {
@@ -18,9 +20,9 @@ class AuthenticateApiToken
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(Request): (RedirectResponse)  $next
+     * @return Response|RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
